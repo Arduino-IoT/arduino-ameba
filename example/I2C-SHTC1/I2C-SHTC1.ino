43c3a35aa30187f6fffac11dea7f3137e7fa6f43
neo_jou <neo_jou@realtek.com>
2015-05-27 14:16:34 +0800

    Add example for I2C with the sensor SHTC1

---
+++ example/I2C-SHTC1/I2C-SHTC1.ino
@@ -0,0 +1,37 @@
+#include "arduino.h"
+#include "shtc1.h"
+#include "rtl_lib.h"
+
+
+void setup()
+{
+  int error;
+  uint16_t shtc1_id;
+ 
+  delay(10000);  
+  DiagPrintf("setup \r\n"); 
+  error = SHTC1_Init(&shtc1_id);
+  if ( error == NO_ERROR ) {
+    DiagPrintf("SHTC1 init ok, id=%d\r\n", shtc1_id);
+  } else {
+    DiagPrintf("SHTC1 init FAILED! \r\n");
+    for(;;);
+  }
+}
+
+
+void loop()
+{
+  int   error;
+  float temperature = 0.0f;
+  float humidity = 0.0f;
+ 
+
+  error = SHTC1_GetTempAndHumi(&temperature, &humidity);
+  
+  rtl_printf("temp=%f, humidity=%f, error=%d\n", 
+            temperature, humidity, error);
+  
+   delay(1000);
+}
+
